3be227565b5cfd257c727b788f8dd9a95a07a4fa
AjayKarthikBoopathy <103437740+AjayKarthikBoopathy@users.noreply.github.com>
2022-05-16 00:45:30 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,13 @@
+//Qn2)Write a person class to hold all the details:-
+class Person {
+  constructor(fname,lname,age,qualification,city,state) {
+    this.fname = fname;
+    this.lname = lname;
+    this.age = age;
+    this.qualification = qualification;
+    this.city = city;
+    this.state = state;
+  }
+}
+  var p1=new Person("Ajay","Karthik","24","B.Tech","Salem","Tamilnadu");
+  console.log(p1);
